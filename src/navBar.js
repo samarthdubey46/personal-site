dde0af5e911f975b3d35d91a73e3c8da54211052
Samarth <samarthdubey46@gmail.com>
2020-09-27 02:31:26 +0530

    Added

--- src/navBar.js
+++ src/navBar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Button, Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import './style.css'
-import logo from '../../Assets/download .svg'
+import logo from '../../Assets/big-logo.svg'
 // import logo from '../../Assets/Logo.svg'
 
 const Home = (props) => {
     return (
-        <div id="home">
-            {/* <div className="logo-div"> */}
+        <div>
+            <div id="home">
                 <img src={logo} />
-            {/* </div> */}
+            </div>
+            <div className="logo-bottom">
+                <div className="about">
+                    <h6 style={{alignSelf:'center',color:'white',fontFamily:'Autowide',fontSize:40}} >Who Am I</h6>
+                </div>
+                {/* <div className="about">
+
+                </div> */}
+            </div>
         </div>
     )
 }
